7f5500e319aa6c48bfae123f8adec936beb9eaf4
Henry La <henryla106@gmail.com>
2022-08-01 12:41:14 -0700

    Added logic for player UI

--- Assets/Scripts/Misc/ScoreManager.cs
+++ Assets/Scripts/Misc/ScoreManager.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager singleton;
-    private TMP_Text healthText;
-    private TMP_Text shurikenText;
-    private TMP_Text scoreText;
+    // private TMP_Text healthText;
+    // private TMP_Text shurikenText;
+    // private TMP_Text scoreText;
     private static int health;
     private static int shurikensRemaining;
     private static int score;
+
+    public List<Texture> healthList;
+    public List<Texture> shurikenList;
+
+    private RawImage curHealthSprite;
+    private RawImage curShurikenSprite;
 
     private void Awake() {
         if (singleton != null && singleton != this) { 
@@ -24,24 +30,32 @@ public class ScoreManager : MonoBehaviour
     }
 
     private void Start() {
-        healthText = GameObject.Find("Health").GetComponent<TMP_Text>();
-        shurikenText = GameObject.Find("Shurikens").GetComponent<TMP_Text>();
-        scoreText = GameObject.Find("Score").GetComponent<TMP_Text>();
+        curHealthSprite = GameObject.Find("Life").GetComponent<RawImage>();
+        curShurikenSprite = GameObject.Find("Shurikens").GetComponent<RawImage>();
     }
     
     public void IncreaseScoreBy(int amount) {
         score += amount;
-        scoreText.text = "Score: " + score;
     }
 
     public void UpdateShurikenNum(int newNum) {
         shurikensRemaining = newNum;
-        shurikenText.text = "Shurikens: " + shurikensRemaining;
+        if (shurikensRemaining == 0) {
+            curShurikenSprite.texture = null;
+            curShurikenSprite.color = new Color(0f, 0f, 0f, 0f);
+        } else {
+            curShurikenSprite.texture = shurikenList[shurikensRemaining - 1];
+        }
     }
 
     public void UpdateHealth(int newHealth) {
         health = newHealth;
-        healthText.text = "HP: " + health;
+        if (health == 0) {
+            curHealthSprite.texture = null;
+            curHealthSprite.color = new Color(0f, 0f, 0f, 0f);
+        } else {
+            curHealthSprite.texture = healthList[health - 1];
+        }
     }
 
 }
