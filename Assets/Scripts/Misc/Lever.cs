2984ed879b726d4a5877aa908713e35ed1728cac
Henry La <72360894+Zenuriken@users.noreply.github.com>
2022-12-30 02:14:26 -0800

    Working on platform trapdoor

--- Assets/Scripts/Misc/Lever.cs
+++ Assets/Scripts/Misc/Lever.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 
 public class Lever : MonoBehaviour
 {
-    private GameObject highLight;
+    // Public Variables.
     public GameObject trapDoors;
+    public Tile tile;
+    public bool isHorizontal;
+
+    // Private Variables.
+    private GameObject highLight;
+    private Vector3Int[] tileList;
+    private Tilemap platformTilemap;
     private Transform leftDoor;
     private Transform rightDoor;
     private Animator anim;
@@ -22,9 +31,14 @@ public class Lever : MonoBehaviour
 
         rightDoor = trapDoors.transform.GetChild(0).transform;
         leftDoor = trapDoors.transform.GetChild(1).transform;
+
+        platformTilemap = GameObject.Find("Tilemap_Platform").GetComponent<Tilemap>();
+
+        SetPlatformTiles();
     }
 
     public void Switch() {
+        // Closing doors.
         if (isOn) {
             anim.SetBool("isOn", false);
             isOn = false;
@@ -32,7 +46,8 @@ public class Lever : MonoBehaviour
             rightDoor.Rotate(0f, 0f, -90f, Space.Self);
             switchSound.Play();
             trapDoorSound.Play();
-
+            TogglePlatformTiles(false);
+        // Opening doors.
         } else {
             anim.SetBool("isOn", true);
             isOn = true;
@@ -40,8 +55,8 @@ public class Lever : MonoBehaviour
             rightDoor.Rotate(0f, 0f, 90f, Space.Self);
             switchSound.Play();
             trapDoorSound.Play();
+            TogglePlatformTiles(true);
         }
-        //Debug.Log("IsOn: " + isOn);
     }
 
     public void SetHighLight(bool status) {
@@ -50,6 +65,36 @@ public class Lever : MonoBehaviour
 
     public void SetMeleeCounter(int num) {
         meleeCounter = num;
+    }
+
+    // Initializes a list of platform tiles that the trapdoor covers.
+    private void SetPlatformTiles() {
+        tileList = new Vector3Int[4];
+        Vector3Int leftTile = platformTilemap.WorldToCell(leftDoor.position);
+        if (isHorizontal) {
+            for (int i = 0; i < 4; i++) {
+                Vector3Int t = new Vector3Int(leftTile.x + i + 1, leftTile.y, leftTile.z);
+                tileList[i] = t;
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                Vector3Int t = new Vector3Int(leftTile.x, leftTile.y + i + 1, leftTile.z);
+                tileList[i] = t;
+            }
+        }
+    }
+
+    // Toggles whethre the platform tiles are present or not.
+    private void TogglePlatformTiles(bool isOn) {
+        if (isOn) {
+            foreach (Vector3Int t in tileList) {
+                platformTilemap.SetTile(t, null);
+            }
+        } else {
+            foreach (Vector3Int t in tileList) {
+                platformTilemap.SetTile(t, tile);
+            }
+        }
     }
 
     // Checks to see if enemy has already been damaged by player's current meleeCounter.
