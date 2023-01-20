7111ab58b9841bc0dd1a9951ada6385f65fa63ba
Henry La <72360894+Zenuriken@users.noreply.github.com>
2023-01-19 23:45:31 -0800

    Finished enemy jump script

--- Assets/Scripts/Enemy/TestLaunch.cs
+++ Assets/Scripts/Enemy/TestLaunch.cs
@@ -4,54 +4,105 @@ using UnityEngine;
 
 public class TestLaunch : MonoBehaviour
 {
-    [SerializeField]
-    float _InitialVelocity;
-    [SerializeField]
-    float _Angle;
-    [SerializeField]
-    LineRenderer _Line;
-    [SerializeField]
-    float _Step;
+    // [SerializeField] float _InitialVelocity;
+    // [SerializeField] float _Angle;
+    // [SerializeField] float _Height;
+    [SerializeField] LineRenderer _Line;
+    [SerializeField] float _Step;
 
+    private Vector3 _InitialPos;
+    private Camera _cam;
     private float _HeightOffset = -1.55f;
 
-    private void Update() {
-        float angle = _Angle * Mathf.Deg2Rad;
-        DrawPath(_InitialVelocity, angle, _Step);
+    private void Start() {
+        _InitialPos = new Vector3(transform.position.x, transform.position.y + _HeightOffset, 0);
+        _cam = Camera.main;
+    }
 
+    private void Update() {
+        Vector3 targetPos = _cam.ScreenToWorldPoint(Input.mousePosition) - _InitialPos;
+        targetPos.z = 0;
+        float height = targetPos.y + targetPos.magnitude / 10f;
+        height = Mathf.Max(0.01f, height);
+        float angle;
+        float v0;
+        float time;
+        CalculatePathWithHeight(targetPos, height, out v0, out angle, out time);
+        
+        DrawPath(v0, angle, time, _Step);
         if (Input.GetKeyDown(KeyCode.Space)) {
             StopAllCoroutines();
-            StartCoroutine(Coroutine_Movement(_InitialVelocity, angle));
+            StartCoroutine(Coroutine_Movement(v0, angle, time));
         }
     }
 
-    private void DrawPath(float v0, float angle, float step) {
+    private void DrawPath(float v0, float angle, float time, float step) {
         step = Mathf.Max(0.01f, step);
-        float totalTime = 10;
-        _Line.positionCount = (int)(totalTime / step) + 2;
+        _Line.positionCount = (int)(time / step) + 2;
         int count = 0;
-        for (float i = 0; i < totalTime; i += step) {
+        for (float i = 0; i < time; i += step) {
             float x = v0 * i * Mathf.Cos(angle);
             float y = v0 * i * Mathf.Sin(angle) - (1f / 2f) * -Physics.gravity.y * Mathf.Pow(i, 2);
-            _Line.SetPosition(count , new Vector3(x, y, 0));
+            _Line.SetPosition(count , _InitialPos + new Vector3(x, y, 0));
             count++;
         }
-        float xfinal = v0 * totalTime * Mathf.Cos(angle);
-        float yfinal = v0 * totalTime * Mathf.Sin(angle) - (1f / 2f) * -Physics.gravity.y * Mathf.Pow(totalTime, 2);
-        _Line.SetPosition(count, new Vector3(xfinal, yfinal, 0));
+        float xfinal = v0 * time * Mathf.Cos(angle);
+        float yfinal = v0 * time * Mathf.Sin(angle) - (1f / 2f) * -Physics.gravity.y * Mathf.Pow(time, 2);
+        _Line.SetPosition(count, _InitialPos + new Vector3(xfinal, yfinal, 0));
     }
 
+    private void CalculatePath(Vector3 targetPos, float angle, out float v0, out float time) {
+        float xt = targetPos.x;
+        float yt = targetPos.y;
+        float g = -Physics.gravity.y;
 
-	IEnumerator Coroutine_Movement(float v0, float angle) {
+        float v1 = Mathf.Pow(xt, 2) * g;
+        float v2 = 2 * xt * Mathf.Sin(angle) * Mathf.Cos(angle);
+        float v3 = 2 * yt * Mathf.Pow(Mathf.Cos(angle), 2);
+        v0 = Mathf.Sqrt(v1 / (v2 - v3));
+
+        time = xt / (v0 * Mathf.Cos(angle));
+
+    }
+
+    private float QuadraticEquation(float a, float b, float c, float sign) {
+        return (-b + sign * Mathf.Sqrt(b * b -4 * a * c)) / (2 * a);
+    }
+
+    private void CalculatePathWithHeight(Vector3 targetPos, float h, out float v0, out float angle, out float time) {
+        float xt = targetPos.x;
+        float yt = targetPos.y;
+        float g = -Physics.gravity.y;
+
+        float b = Mathf.Sqrt(2 * g * h);
+        float a = (-0.5f * g);
+        float c = -yt;
+
+        float tplus = QuadraticEquation(a, b, c, 1);
+        float tmin = QuadraticEquation(a, b, c, -1);
+        time = tplus > tmin ? tplus : tmin;
+
+        angle = Mathf.Atan(b * time / xt);
+
+        v0 = b / Mathf.Sin(angle);
+
+        float v1 = Mathf.Pow(xt, 2) * g;
+        float v2 = 2 * xt * Mathf.Sin(angle) * Mathf.Cos(angle);
+        float v3 = 2 * yt * Mathf.Pow(Mathf.Cos(angle), 2);
+        v0 = Mathf.Sqrt(v1 / (v2 - v3));
+
+        time = xt / (v0 * Mathf.Cos(angle));
+    }
+
+	IEnumerator Coroutine_Movement(float v0, float angle, float time) {
         float t = 0;
-        while (t < 100) {
+        while (t < time) {
             float x = v0 * t * Mathf.Cos(angle);
             float y = v0 * t * Mathf.Sin(angle) - (1f / 2f) * -Physics.gravity.y * Mathf.Pow(t, 2);
 
-            transform.position = new Vector3(x, y, 0);
+            transform.position = _InitialPos + new Vector3(x, y - _HeightOffset, 0);
             t += Time.deltaTime;
             yield return null;
-
         }
     }
     
